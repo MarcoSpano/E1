492e16f3d3e6311e90f9f6679d405d4f29757ded
MarcoSpano <marco.spano@studenti.unitn.it>
2018-01-25 16:43:04 +0100

    post is now available

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const uuid = require('uuid-v4');
 const bodyparser = require('body-parser');
 
 app.use(cors());
@@ -9,25 +8,27 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
 var port = process.env.PORT || 8080;
-var astronauts = [];
-var id = 0;
+var assignments = [];
 
-app.get('/simpleGet', (req,res) => {
+app.get('/getAssignment', (req,res) => {
 
-	res.json(astronauts);
+	res.json(assignments);
 	
 });
 
-app.post('/addAstronaut', (req,res) => {
-	id++;
+app.post('/addAssignment', (req,res) => {
 
-	//Console.log(e);
+	taskid = req.body.taskID;
+	assignemntid = req.body.assignmentID;
+	workerid = req.body.workerID;
+	assignmentres = req.body.assignemntResult;
 
-	astronauts.push({"id": id,"firstName": req.body.firstName, lastName: req.body.lastName, isInSpace: req.body.isInSpace});
-	res.sendStatus('SUCCESS');
+	assignments.push({"taskID": taskid,"assignmentID": assignemntid, "workerID": workerid, "assignemntResult": assignmentres});
+	//res.sendStatus('SUCCESS');
+	res.json({"taskID": taskid,"assignmentID": assignemntid, "workerID": workerid, "assignemntResult": assignmentres});
 });
 
-app.get('/findAstronaut/:id', (req,res) => {
+/*app.get('/findAstronaut/:id', (req,res) => {
 	var id = req.params.id;
 
 	var first = req.body.firstName;
@@ -48,6 +49,6 @@ app.put('/put/:id', (req,res) => {
 	atronauts[i] = {"id": i,"firstName": req.body.firstName, lastName: req.body.lastName, isInSpace: req.body.isInSpace};
 });
 
-
+*/
 app.listen(port);
 console.log("Listen to port "+port);
